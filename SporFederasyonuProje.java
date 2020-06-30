ba1eb5e4823f1a8c36660236a5415f70f66e2b66
ibd <37176057+ihsanbakidogann@users.noreply.github.com>
2020-06-30 21:33:25 +0300

    Update SporFederasyonuProje.java

--- SporFederasyonuProje.java
+++ SporFederasyonuProje.java
@@ -1,21 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package sporfederasyonu.proje;
+package tbf;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author kerem ayseli
- */
-public class SporFederasyonuProje {
+public class TurkiyeBasketbolFederasyonu {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        Kaleci kale = new Kaleci();
        Sporcular sporcu = new Sporcular();
